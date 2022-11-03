efa14a8b303d5b0eeb4d1c7a6f1666b98bed73d2
strontiumsu <strontiumsu@gmail.com>
2022-11-03 12:36:19 -0700

    added in everything

--- aomRepumpers_test.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jun  3 13:00:34 2022
-
-@author: sr
-"""
-
--- safety.py
+++ safety.py
@@ -4,24 +4,40 @@ Created on Mon Apr 18 18:11:36 2022
 
 @author: sr
 """
+import sys
+sys.path.append("C:/Users/sr/Documents/Artiq/artiq-master/repository/Classes")
 
 from artiq.experiment import *
 import numpy as np    
-from MOTcoils import* 
+from MOTCoilsClass import _MOTCoils
+from Beamline461Class import _Beamline461
 
 
 class Safety(EnvExperiment):
     
     def build(self): 
         self.setattr_device("core")
-        self.MC=MOTcoils(self)
-    
-    @kernel    
-    def run(self):
+        self.setattr_device('ttl7')
+        self.MC=_MOTCoils(self)
+        self.BB=_Beamline461(self)
+        self.setattr_device("scheduler")
 
-        self.core.reset()
+    
+    #@kernel    
+    def run(self):
         
-        self.ttl5.on()
+        while 1:
+            self.core.comm.close()
+            self.scheduler.pause()
+            self.safety_settings()
+    
+    @kernel        
+    def safety_settings(self):
+        self.core.reset()
         self.MC.init_DAC()   # Initialize MOT coil DAC
         delay(500*ms)
         self.MC.Zero_current()    
+        delay(100*ms)
+        self.BB.MOT_off()
+        delay(100*ms)
+        self.ttl7.off()
